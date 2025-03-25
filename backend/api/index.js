391cb8c27f77bae7fcade844f51fb90e05bd434f
seonyeonghun2 <kjca1117@naver.com>
2025-03-25 12:44:40 +0900

    backend | api/index.js
    1. crud-read.js : code 병합
    
    2. vercel environment variable : MONGODB_ATLAS_URI 추가

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,18 @@
+import { MongoClient } from "mongodb";
 export async function GET(request) {
-    // return new Response('Hello from Vercel!');
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-  const data = await res.json();
-  return response.status(200).json({ data });
+  const uri = MONGODB_ATLAS_URI;
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    const db = client.db("hr");
+    const coll = db.collection("employees");
+    const cursor = coll.find(); // .limit(10) - 사용자 정보 10개 제한 (총 21,349개)
+    const users = await cursor.toArray();
+    return users;
+  } catch (e) {
+    console.log(e);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
+}
