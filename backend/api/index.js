160eb19012b63e4fef9b755e068c9d4b2fa19f0f
seonyeonghun2 <kjca1117@naver.com>
2025-03-25 12:36:11 +0900

    update sample data fetch

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,6 @@
-export function GET(request) {
-    return new Response('Hello from Vercel!');
+export async function GET(request) {
+    // return new Response('Hello from Vercel!');
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await res.json();
+  return response.status(200).json({ data });
   }
