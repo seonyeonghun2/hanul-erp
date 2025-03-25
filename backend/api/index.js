e16c667d2aecd5e5835bd50e2574c806a4396471
seonyeonghun2 <kjca1117@naver.com>
2025-03-25 12:49:12 +0900

    backend | api/index.js
    1. vercel function : get, post, put, delete 테스트

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,18 +1,12 @@
-import { MongoClient } from "mongodb";
-export async function GET(request) {
-  const uri = MONGODB_ATLAS_URI;
-  const client = new MongoClient(uri);
-  try {
-    await client.connect();
-    const db = client.db("hr");
-    const coll = db.collection("employees");
-    const cursor = coll.find(); // .limit(10) - 사용자 정보 10개 제한 (총 21,349개)
-    const users = await cursor.toArray();
-    return users;
-  } catch (e) {
-    console.log(e);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+export function GET(request) {
+  return new Response('response GET');
+}
+export function POST(request) {
+  return new Response('response POST');
+}
+export function PUT(request) {
+  return new Response('response PUT');
+}
+export function DELETE(request) {
+  return new Response('response DELETE');
 }
