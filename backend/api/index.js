88c91b99daf4c17287d678e7f8d0a2c50446e1d8
seonyeonghun2 <kjca1117@naver.com>
2025-03-26 09:53:06 +0900

    url base | rewrites

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,9 +4,3 @@ export function GET(request) {
 export function POST(request) {
   return new Response('response POST');
 }
-export function PUT(request) {
-  return new Response('response PUT');
-}
-export function DELETE(request) {
-  return new Response('response DELETE');
-}
