b80c0ec26a14bd996ffe023f219dde23f849032e
seonyeonghun2 <kjca1117@naver.com>
2025-03-26 10:44:38 +0900

    backed | get, post 요청 처리
    1. req.method에 따른 switch ~ case
    2. mongodb 연동 : environment variable

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,36 @@
-export function GET(request) {
-  return new Response('response GET');
-}
-export function POST(request) {
-  return new Response('response POST');
+import { MongoClient } from "mongodb";
+export default async function handler(req, res) {
+  // 몽고db 클라이언트 URI
+  const client = new MongoClient(process.env.MONGODB_URI);
+  // 몽고DB 클라이언트 연결 수립
+  await client.connect();
+  const db = client.db("hr");
+  const coll = db.collection("employees");
+
+  // GET 요청 or POST 요청 인지에 따라 서로다른 데이터베이스 요청 처리
+
+  try {
+    switch(req.method){
+      case 'GET':
+        const allEmployees = await coll.find().toArray()
+        res.status(200).json(allEmployees);
+        break;
+      case 'POST':
+        const newEmployee= req.body;
+        const result = await coll.insertOne(newEmployee);
+        res.status(200).json(result);
+        break;
+      default:
+        res.setHeader('Allow', ['GET','POST','PUT','DELETE']);
+        res.status(405).end(`${req.method}는 허용되지 않습니다!`)
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      error: e.message,
+      msg: '서버 통신 오류, 관리자 문의 요망!'      
+    })
+  } finally {
+    await client.close();
+  }
 }
